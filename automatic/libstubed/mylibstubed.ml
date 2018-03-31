56c5fc33575fae7154f0b8a8f5aea727b6fbc0ff
Matthieu Dubuget <matthieu.dubuget@gmail.com>
2018-03-31 17:05:43 +0200

    Update mylibstubed.ml
    Comment added

--- automatic/libstubed/mylibstubed.ml
+++ automatic/libstubed/mylibstubed.ml
@@ -1,4 +1,7 @@
+
+(* Never used external, to force C stub to be linked *)
 external dummy: unit -> unit = "dummy"
+
 let () =
   Callback.register "mylib_add" Mylib.add;
   Callback.register "mylib_sub" Mylib.substract
